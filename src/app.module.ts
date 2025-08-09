b556d576f7aede372a6ab9c84addd13a17426cd8
mansurjr <ztm007uzb@gmail.com>
2025-08-09 16:52:36 +0500

    Updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,29 @@
 import { Module } from "@nestjs/common";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
+
 import { StudentModule } from "./student/student.module";
 import { PrismaService } from "./prisma/prisma.service";
 import { PrismaModule } from "./prisma/prisma.module";
-import { AuthModule } from './auth/auth.module';
-import { ShopItemModule } from './shop-item/shop-item.module';
-import { TelegramModule } from './telegram/telegram.module';
+import { AuthModule } from "./auth/auth.module";
+import { ShopItemModule } from "./shop-item/shop-item.module";
+import { TelegramModule } from "./telegram/telegram.module";
 
 @Module({
-  imports: [StudentModule, PrismaModule, AuthModule, ShopItemModule, TelegramModule, TelegramModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "..", "uploads"), // absolute path to your uploads folder
+      serveRoot: "/uploads", // URL prefix
+      serveStaticOptions: {
+        index: false,
+      },
+    }),
+    StudentModule,
+    PrismaModule,
+    AuthModule,
+    ShopItemModule,
+    TelegramModule,
+  ],
   controllers: [],
   providers: [PrismaService],
 })
